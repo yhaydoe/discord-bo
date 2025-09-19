1967e600075af41c8d562a941ba2e09a064518c4
yhaydoe <e0ec78b99da@gmail.com>
2025-09-19 13:57:26 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
